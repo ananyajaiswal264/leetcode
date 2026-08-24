545593337190910e93f3aca077d1d4c7ef24707b
ananyajaiswal264 <156197563+ananyajaiswal264@users.noreply.github.com>
2026-08-24 14:26:17 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.34%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n =nums.size();
+        int maxIdx=0;
+        for(int i=0;i<n;i++){
+           if(i<=maxIdx) maxIdx=max(maxIdx,i+nums[i]);
+        }
+        if(maxIdx>=n-1) return true;
+        return false;
+        
+    }
+};
